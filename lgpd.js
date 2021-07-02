f5a19c8778d63e0a98ee7a0cc8f4cee5e7b549bf
THONWELLING <thonwellingdani13@gmail.com>
2021-07-02 19:37:09 -0300

    pimeiro commit

---
+++ lgpd.js
@@ -0,0 +1,33 @@
+let lgpdUrl = 'https://www.jsonplaceholder.typicode.com/posts';
+let lgpdHtml = `
+<div class="lgpd">
+    <div class="lgpd--left">
+        Nós ultilizamos cookies para melhorar a sua  experiência de usuário.<br>
+        Para conferir detalhadamente todos os cookies utilizados,leia a nossa <a href=''>política de privacidade</a>.
+    </div>
+    <div class="lgpd--right">
+        <button>ok</button>
+    </div>
+</div>
+<link rel="stylesheet" href="lgpd.css" />
+`;
+
+let lsContent = localStorage.getItem('lgpd');
+if(!lsContent) {
+    document.body.innerHTML += lgpdHtml;
+
+    let lgpdArea = document.querySelector('.lgpd');
+    let lgpdButton = lgpdArea.querySelector('button');
+
+    lgpdButton.addEventListener('click', async () =>{
+        lgpdArea.remove();
+
+        let result = await fetch(lgpdUrl);
+        let json = await result.json();
+
+        if(json.error != '') {
+            let id = '123'; // aqui normalmente seria json.id;
+            localStorage.setItem('lgpd', id);
+        }
+    });
+}
